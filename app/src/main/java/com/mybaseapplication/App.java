153c1eba72337dbf9cd9b6dc6de236709db42615
bsw_lm <https://git.oschina.net/bsw_lm/mybaseapplication.git>
2017-07-02 17:40:07 +0800

    目录

---
+++ app/src/main/java/com/mybaseapplication/App.java
@@ -0,0 +1,11 @@
+package com.mybaseapplication;
+
+import android.app.Application;
+
+/**
+ * Created by dell on 2017/7/2.
+ */
+
+public class App extends Application {
+
+}
--- app/src/main/java/com/mybaseapplication/ui/activity/MainActivity.java
+++ app/src/main/java/com/mybaseapplication/ui/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.mybaseapplication;
+package com.mybaseapplication.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.mybaseapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
