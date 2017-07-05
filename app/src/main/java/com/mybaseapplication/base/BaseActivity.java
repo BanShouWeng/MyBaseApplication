3ded0b3cf15638b4d757eb2a20ca82314c058941
bsw_lm <https://git.oschina.net/bsw_lm/mybaseapplication.git>
2017-07-05 17:15:35 +0800

    base(二)

--- app/src/main/java/com/mybaseapplication/base/BaseActivity.java
+++ app/src/main/java/com/mybaseapplication/base/BaseActivity.java
@@ -1,15 +1,56 @@
 package com.mybaseapplication.base;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 import com.mybaseapplication.R;
 
 public class BaseActivity extends AppCompatActivity {
 
+    public Activity context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
+        context = this;
+    }
+
+    public void setBaseContentView(int layoutId) {
+        ((ScrollView)findViewById(R.id.base_scroll_view)).setFillViewport(true);
+        LinearLayout layout = (LinearLayout) findViewById(R.id.base_main_layout);
+        LayoutInflater inflater = getLayoutInflater();
+        final View view = inflater.inflate(layoutId, null);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        layout.addView(view, params);
+    }
+
+//        ButterKnife.bind(this);
+//        view.setOnTouchListener(new View.OnTouchListener() {
+//
+//            @Override
+//            public boolean onTouch(View arg0, MotionEvent arg1) {
+//                hideKeyBoard();
+//                view.setFocusable(true);
+//                view.setFocusableInTouchMode(true);
+//                return false;
+//            }
+//        });
+
+    public void hideKeyBoard() {
+        View view = ((Activity) context).getWindow().peekDecorView();
+        if (view != null) {
+            InputMethodManager inputmanger = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputmanger.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
     }
 }
--- app/src/main/java/com/mybaseapplication/ui/activity/MainActivity.java
+++ app/src/main/java/com/mybaseapplication/ui/activity/MainActivity.java
@@ -1,25 +1,15 @@
 package com.mybaseapplication.ui.activity;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 import com.mybaseapplication.R;
 import com.mybaseapplication.base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        findViewById(R.id.text).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, BaseActivity.class));
-            }
-        });
+        setBaseContentView(R.layout.activity_main);
     }
 }
