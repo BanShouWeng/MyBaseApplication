db2652b30ac35bad8f84aebca1403f7064751407
bsw_lm <https://git.oschina.net/bsw_lm/mybaseapplication.git>
2017-12-17 08:46:00 +0800

    gradle.properties与build.gradle配合设置BuildConfig参数

--- app/src/main/java/com/banshouweng/mybaseapplication/base/activity/BaseActivity.java
+++ app/src/main/java/com/banshouweng/mybaseapplication/base/activity/BaseActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
+
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
